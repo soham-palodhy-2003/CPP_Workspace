8013e811da2d586ecdf0213f150ff1c9b255626a
Soham Palodhy <sohampalodhy8068@gmail.com>
2024-09-19 17:09:56 +0000

    Adding patterns

--- Patterns/pattern2.cpp
+++ Patterns/pattern2.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main(){
     int n;
+    int num = 1;
     cout<<"Enter the no of Rows: ";
     cin>>n;
 
     for( int i = 1; i<=n ; i++){
         for (int j = 1; j <= n ; j++){
-            cout << j;
+            cout << num  <<" ";
+            num++;
         }
-        cout <<endl;   //1234
-    }                  //1234
-    return 0;          //1234
-}                      //1234
+        cout <<endl;   //1 2 3
+    }                  //4 5 6
+    return 0;          //7 8 9
+}                      //10 11 12
